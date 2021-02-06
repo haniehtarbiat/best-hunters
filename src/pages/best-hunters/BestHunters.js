c091bcfe8fae2d6ecb372126f360fe73ac54cb52
unknown <tarbiathanieh@gmail.com>
2021-02-06 16:24:18 +0330

    move header component to page header

--- src/pages/best-hunters/BestHunters.js
+++ src/pages/best-hunters/BestHunters.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styles from 'pages/best-hunters/BestHunters.module.css';
-import Header from 'pages/best-hunters/components/Header/Haeder';
 
 function BestHunters() {
     return (
         <div className={styles.container}>
-            <Header />
+            <header>
+                <h1 className={styles.heading}>برترین ها</h1>
+            </header>
+            <div className={styles.filterContainer}>
+                <p>با شکارچیان برتر در بازه‌ی زمانی دلخواهتون آشنا بشید.</p>
+                <ul>
+                    <li>مهر-آبان ۱۳۹۹ </li>
+                    <li>آذر-دی ۱۳۹۹ </li>
+                    <li>بهمن-اسفند ۱۳۹۹ </li>
+                    <li>همیشه</li>
+                </ul>
+            </div>
         </div>
     );
 }
